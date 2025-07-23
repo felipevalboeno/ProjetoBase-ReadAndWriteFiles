40ef61b937613efc2a1435ce654803b0ef6fbe04
Felipe Valboeno <feliperocha14@hotmail.com>
2025-07-23 10:00:53 -0300

    Primeiro commit - projeto criado

---
+++ src/java_nio_simple/JavaNIO1.java
@@ -0,0 +1,28 @@
+package java_nio_simple;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class JavaNIO1 {
+
+    public static void main(String[] args) {
+        try{
+
+            Path path = Paths.get("C:\\arquivos\\java-nio.txt");
+
+            //Lendo o path e convertendo todos os caracteres de uma só vez
+            byte[] bytesArquivo = Files.readAllBytes(path);
+
+
+            // como são bytes, podemos criar uma string agora
+            String conteudo = new String(bytesArquivo);
+            System.out.println(conteudo);
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+
+        }
+
+    }
+}
